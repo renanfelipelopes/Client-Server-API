cf443061cc6634da7f9d4b8ae1b1e7237e1510ce
Renan Lopes <renanlopes.ads@gmail.com>
2026-03-02 22:13:40 -0300

    feat: desenvolvendo client.go

--- client.go
+++ client.go
@@ -1,1 +1,33 @@
 package main
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"time"
+)
+
+func main() {
+	ctx, cancel := context.WithTimeout(context.Background(), 300*time.Millisecond)
+	defer cancel()
+
+	req, err := http.NewRequestWithContext(
+		ctx,
+		"GET",
+		"http://localhost:8080/cotacao",
+		nil,
+	)
+	if err != nil {
+		fmt.Println("Erro ao criar requisição:", err)
+		return
+	}
+
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		fmt.Println("Erro na requisição:", err)
+		return
+	}
+	defer res.Body.Close()
+
+	fmt.Println("Requisição executada com sucesso")
+}
